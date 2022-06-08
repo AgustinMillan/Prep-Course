adfc050620f6999a0bcd363387c22490a6e49fb0
AgustinMillan <victor.agustin.millan@gmail.com>
2022-06-08 16:47:24 -0300

    JSIV finish

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -123,7 +123,7 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  if (arguments.length < 1) return 0;
+  if (arguments.length < 1){ return 0;}
   var x = 1;
   for (var i = 0; i < arguments.length; i++){
     x = x * arguments[i];
